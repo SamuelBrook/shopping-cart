65d2d312d08be00458b2fb2145358a12c5afa97a
Samuel Brook <samuelbhbrook@gmail.com>
2022-05-18 21:46:28 +0200

    Add elemtents to navigation branch
    Includes title, home, shop and shopping cart. Give them appropriate
    class names.

---
+++ src/components/Nav.js
@@ -0,0 +1,8 @@
+function Nav() {
+  <div className="navBar">
+    <div className="navBar-header">MockShop</div>
+    <div className="navBar-home">Home</div>
+    <div className="navBar-shop">Shop</div>
+    <div className="navBar-shoppingCart"></div>
+  </div>;
+}
