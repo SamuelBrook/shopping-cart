b221dd185903b7cbfffbfa59dcaf670d971bb5be
Samuel Brook <samuelbhbrook@gmail.com>
2022-07-01 21:16:57 +0100

    Add image and style main page
    Wig wam image added to main page and container added to allow for
    separation between text and image. Styles added to main page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,29 @@
 import { Link } from "react-router-dom";
 import "../styles/Home.css";
+import wigwams from "../assets/images/wigwams.jpg";
 
 function Home() {
   return (
     <main>
-      <div>
-        <div>Top Online Store of 2022</div>
-        <div>We don't just make tents, we make art</div>
-        <button className="shop-link">
-          <Link to="/shop">Shop now</Link>
-        </button>
-      </div>
-      <div>
-        <img></img>
+      <div className="container">
+        <div className="container-textContainer">
+          <div className="container-textContainer-headline">
+            Top Online Store of 2022
+          </div>
+          <div className="container-textContainer-statement">
+            We don't just make tents, we make art
+          </div>
+          <button className="shop-link">
+            <Link to="/shop">Shop now</Link>
+          </button>
+        </div>
+        <div className="container-imageContainer">
+          <img
+            alt="wigwams under a night sky"
+            src={wigwams}
+            className="container-imageContainer-image"
+          ></img>
+        </div>
       </div>
     </main>
   );
